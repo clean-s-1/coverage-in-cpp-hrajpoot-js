27e272a3ff2d3ff9461fe29dfee4cac95432cac4
hrajpoot-js <65027195+hrajpoot-js@users.noreply.github.com>
2022-07-07 16:46:12 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,41 +4,6 @@
 #include "typewise-alert.h"
 #include "limit_breach.h"
 
-TEST_CASE("infers the breach according to limits") {
-  Breach breachObj;
-  REQUIRE(breachObj.inferBreach(12, 20, 30) == TOO_LOW);
-}
-
-TEST_CASE("case 2: classify the temperature breach") {
-  Breach breachObj;
-  REQUIRE(breachObj.classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
-}
-
-TEST_CASE("case 3: classify the temperature breach") {
-  Breach breachObj;
-  REQUIRE(breachObj.classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
-}
-
-TEST_CASE("case 5: classify the temperature breach") {
-  Breach breachObj;
-  REQUIRE(breachObj.classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
-}
-
-TEST_CASE("case 6: classify the temperature breach") {
-  Breach breachObj;
-  REQUIRE(breachObj.classifyTemperatureBreach(PASSIVE_COOLING, 46) == TOO_HIGH);
-}
-
-TEST_CASE("case 8: classify the temperature breach") {
-  Breach breachObj;
-  REQUIRE(breachObj.classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
-}
-
-TEST_CASE("case 9: classify the temperature breach") {
-  Breach breachObj;
-  REQUIRE(breachObj.classifyTemperatureBreach(MED_ACTIVE_COOLING, 41) == TOO_HIGH);
-}
-
 TEST_CASE("case 1: check and alert") {
   Breach breachObj;
   BatteryCharacter batteryCharacter;
