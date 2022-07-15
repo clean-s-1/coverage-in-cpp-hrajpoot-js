8198d57489938fe83d4ecd55137a7863ad393b4f
Hardev <65027195+hrajpoot-js@users.noreply.github.com>
2022-07-15 12:34:44 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -7,6 +7,7 @@ class Alerter
     public:
     void sendToController(BreachType breachType);
     void sendToEmail(BreachType breachType);
+    private:
     void composeAnEmail(const char* recepient, const char* message);
     void composeControllerMsg(BreachType breachType, const unsigned short header);
 };
